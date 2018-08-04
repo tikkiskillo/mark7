b1df6dbdf44fdccd22591ca527d503a9301bd90d
Patric Pereira <patric.pereirafranca@gmail.com>
2018-08-04 12:39:16 -0300

    incluindo headless

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,6 +2,7 @@ pipeline {
   agent {
     docker {
       image 'ruby'
+      args '--link selenium'
     }
   }
   environment {
@@ -15,7 +16,7 @@ pipeline {
     }
     stage('Run Features') {
       steps {
-        sh "bundle exec cucumber"
+        sh "bundle exec cucumber -p ci"
       }
     }
   }
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,30 @@
 require 'capybara'
 require 'capybara/cucumber'
 
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+  Capybara.javascript_driver = :selenium
+  Capybara.run_server = false
+
+  args = ['--no-default-browser-check']
+
+  caps = Selenium::Webdriver::Remote::Capabilities.chrome(
+    'chromeOptions' => {
+      'args' => args
+    }
+  )
+
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :remote,
+      url: 'https://selenium:4444/wd/hub',
+      desired_capabilities: caps
+    )
+  end
+end
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = 'https://mark7.herokuapp.com'
